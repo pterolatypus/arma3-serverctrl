e6d74a333cf488e01a301d96bbbade3fa989bdee
Pterolatypus <pterolatypus@gmail.com>
2018-03-02 19:41:08 +0000

    Added new entry point

---
+++ serverctrl.bat
@@ -0,0 +1,10 @@
+rem default values of program variables
+SET STEAMUSER=username
+SET STEAMPASS=password
+SET ARMADIR=a3master
+SET BRANCH=233780 -beta
+SET STEAMDIR=steam
+SET MODFILE=modlist.txt
+
+rem start the application
+CALL scripts\main.bat
